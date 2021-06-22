f925988d1eae3ba469f35043f949e63c327b7965
Nicholas J. Benoit <redeagletwo@gmail.com>
2021-06-22 18:28:54 +0900

    Changes to English html pages and global css files

--- README.md
+++ README.md
@@ -12,6 +12,11 @@ Notes to be shared. Feel free to write down. （共有すべきメモなど）
 * Make contents: news, schedule, member(and staff pages?), access,...
 * Separate data(javascript, css,...) from html files
 
+# Suggestions
+* Responsive Design, Flexbox and Grid　（レスポンシブデザイン、フレックスボックスとグリッド）
+* Use Grid for body of page.  Then use mix of Flexbox and Grid for internal elements
+* I think the tailwind.css is too large of a file and we should create our own.
+
 
 <!-- * `src/templates/*.ejs`からテンプレートファイルを読み取る。 -->
 <!-- * `js`でテンプレートファイルの各要素を埋める。 -->
--- scripts/backup_main.js
+++ scripts/backup_main.js
@@ -3,36 +3,30 @@
 // Read in the membership data though and xml request, should be formatted as .csv file
 $.get('/scripts/members.csv', (data)=>{
     var memberData = $.csv.toArrays(data);
-    generateHTML(memberData);
+    generateHTML(memberData.flat(1));
 });
 
 // Generate the html div data to be appended to <div class="px-12 py-16 w-full">
 function generateHTML(memberData){
-    const titles = document.querySelector('#titles');
+    let n = 0;
+    const titles = document.querySelector('#studentinfo'); // pull in the different html locations
 
-    let divName = document.createElement('div');
-    divName.className = 'col-span-3';
-    divName.innerHTML = memberData[0][0];
-    titles.appendChild(divName);
-
-    let divTitle = document.createElement('div');
-    divTitle.className = 'col-span-1';
-    divTitle.innerHTML = memberData[0][1];
-    titles.appendChild(divTitle);
-
-    let divField = document.createElement('div');
-    divField.className = 'col-span-3';
-    divField.innerHTML = memberData[0][2];
-    titles.appendChild(divField);
-
-    let divEmail = document.createElement('div');
-    divEmail.className = 'col-span-2';
-    divEmail.innerHTML = memberData[0][3];
-    titles.appendChild(divEmail);
-
-    let divRoom = document.createElement('div');
-    divRoom.className = 'col-span-1';
-    divRoom.innerHTML = memberData[0][4];
-    titles.appendChild(divRoom);
+    while (n < memberData.length) {
+        let divName = document.createElement('div');  // what html to make
+        //divName.className = 'col-auto';
+        divName.className = 'col-auto justify-center'; //justify-center appears to be broken
+        divName.innerHTML = memberData[n];
+        titles.appendChild(divName);
+        n++;
+    };
+//<div id="names" class="grid grid-cols-10 gap-2 h-12 py-2 align-center justify-center">
+//        <div class="col-span-3 align-center justify-center">
+//            <span class="rounded-full h-10 w-10 flex items-center justify-center bg-blue-900 inline-flex">C</span>
+//            <span class="ml-3">松尾　大和</span>
+//        </div>
+//        <div class="col-span-1 align-center justify-center">D2</div>
+//        <div class="col-span-3 align-center justify-center">格子QCD、素粒子物理学</div>
+//        <div class="col-span-2 align-center justify-center">y-matsuo@hiroshima-u.ac.jp</div>
+//        <div class="col-span-1 align-center justify-center">B201</div>
 };
 
